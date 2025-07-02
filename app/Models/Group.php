34ad0d7cc6b71b38d641ab07ab7ef62e207e5c71
Judy Abraham <mrjudyabraham@gmail.com>
2025-07-02 20:51:36 +0530

    Updated Models with required Protected Fillables and added appropriate relationship functions between respective models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,9 +2,52 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+        'last_message_id',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(user::class, 'group_users');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function attachments()
+    {
+        return $this->hasMany(MessageAttachment::class);
+    }
 }
